52e0292816f9382f36b30c99dff93efe40bf4422
Alex Gulliver <randdalf@gmail.com>
2021-12-10 22:32:16 +0000

    Day 10, Puzzle 2

--- d10-test.py
+++ d10-test.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from d10 import parse, syntax_errors
+from d10 import parse, syntax_errors, autocomplete
 
 example1 = """[({(<(())[]>[[{[]{<()<>>
 [(()[<>])]({[<{<<[]>>(
@@ -21,6 +21,11 @@ example1 = """[({(<(())[]>[[{[]{<()<>>
 class SyntaxErrorsTests(unittest.TestCase):
     def test_example1(slf):
         slf.assertEqual(syntax_errors(parse(example1)), 26397)
+
+
+class AutocompleteTests(unittest.TestCase):
+    def test_example1(slf):
+        slf.assertEqual(autocomplete(parse(example1)), 288957)
 
 
 if __name__ == "__main__":
--- d10.py
+++ d10.py
@@ -2,13 +2,22 @@
 
 """Advent of Code 2021, Day 10"""
 
+from functools import reduce
+
 from aoc import solve
 
-scores = {
+error_scores = {
     ')': 3,
     ']': 57,
     '}': 1197,
     '>': 25137
+}
+
+autocomplete_scores = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4
 }
 
 opens = {
@@ -21,24 +30,61 @@ opens = {
 closes = {v: k for k, v in opens.items()}
 
 
+class CorruptedError(Exception):
+    def __init__(slf, c):
+        slf.c = c
+
+
+class IncompleteError(Exception):
+    def __init__(slf, stack):
+        slf.missing = [opens[c] for c in reversed(stack)]
+
+
 def parse(data):
     return data.split('\n')
+
+
+def process(line):
+    stack = []
+    for c in line:
+        if c in opens:
+            stack.append(c)
+        elif stack[-1] != closes[c]:
+            raise CorruptedError(c)
+        else:
+            stack.pop()
+    if len(stack) > 0:
+        raise IncompleteError(stack)
 
 
 def syntax_errors(lines):
     total_score = 0
     for line in lines:
-        stack = []
-        for c in line:
-            if c in opens:
-                stack.append(c)
-            elif stack[-1] != closes[c]:
-                total_score += scores[c]
-                break
-            else:
-                stack.pop()
+        try:
+            process(line)
+        except CorruptedError as err:
+            total_score += error_scores[err.c]
+        except IncompleteError:
+            pass
     return total_score
 
 
+def autocomplete(lines):
+
+    def score(a, c):
+        return a * 5 + autocomplete_scores[c]
+
+    scores = []
+    for line in lines:
+        try:
+            process(line)
+        except IncompleteError as err:
+            scores.append(reduce(score, err.missing, 0))
+        except CorruptedError:
+            pass
+    scores.sort()
+    return scores[len(scores) // 2]
+
+
 if __name__ == "__main__":
-    solve(10, parse, syntax_errors)
+    solve(10, parse, syntax_errors, autocomplete)
