cbd9647be42dd302fb490d83d8a58fad8f8e469a
Alex Gulliver <randdalf@gmail.com>
2021-12-13 18:23:52 +0000

    Day 13, Puzzle 2

--- d13.py
+++ d13.py
@@ -33,13 +33,28 @@ def fold_y(dot, n):
     return (dot[0], fold_axis(dot[1], n))
 
 
-def count_dots(manual, limit=1):
+def fold_paper(manual, limit=None):
     dots, instrs = manual
     for axis, n in instrs[:limit]:
         fold = fold_x if axis == 'x' else fold_y
         dots = {fold(dot, n) for dot in dots}
-    return len(dots)
+    return dots
+
+
+def count_dots(manual):
+    return len(fold_paper(manual, 1))
+
+
+def print_code(manual):
+    dots = fold_paper(manual)
+    size = tuple(1 + max(dot[i] for dot in dots) for i in range(2))
+    code = ''
+    for y in range(size[1]):
+        for x in range(size[0]):
+            code += ''.join('#' if (x, y) in dots else ' ')
+        code += '\n'
+    return code
 
 
 if __name__ == "__main__":
-    solve(13, parse, count_dots)
+    solve(13, parse, count_dots, print_code)
