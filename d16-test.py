791532ee3f612c5626411549c0862eb388b2d34e
Alex Gulliver <randdalf@gmail.com>
2021-12-16 21:31:20 +0000

    Day 16, Puzzle 2

--- d16-test.py
+++ d16-test.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from d16 import parse, version_sum
+from d16 import parse, version_sum, evaluate
 
 example1 = "D2FE28"
 example2 = "38006F45291200"
@@ -13,6 +13,15 @@ example4 = "8A004A801A8002F478"
 example5 = "620080001611562C8802118E34"
 example6 = "C0015000016115A2E0802F182340"
 example7 = "A0016C880162017C3686B18A3D4780"
+
+example8 = "C200B40A82"
+example9 = "04005AC33890"
+example10 = "880086C3E88112"
+example11 = "CE00C43D881120"
+example12 = "D8005AC2A8F0"
+example13 = "F600BC2D8F"
+example14 = "9C005AC2F8F0"
+example15 = "9C0141080250320F1802104A08"
 
 
 class VersionSumTests(unittest.TestCase):
@@ -36,6 +45,35 @@ class VersionSumTests(unittest.TestCase):
 
     def test_example7(slf):
         slf.assertEqual(version_sum(parse(example7)), 31)
+
+
+class EvaluateTests(unittest.TestCase):
+    def test_example1(slf):
+        slf.assertEqual(evaluate(parse(example1)), 2021)
+
+    def test_example8(slf):
+        slf.assertEqual(evaluate(parse(example8)), 3)
+
+    def test_example9(slf):
+        slf.assertEqual(evaluate(parse(example9)), 54)
+
+    def test_example10(slf):
+        slf.assertEqual(evaluate(parse(example10)), 7)
+
+    def test_example11(slf):
+        slf.assertEqual(evaluate(parse(example11)), 9)
+
+    def test_example12(slf):
+        slf.assertEqual(evaluate(parse(example12)), 1)
+
+    def test_example13(slf):
+        slf.assertEqual(evaluate(parse(example13)), 0)
+
+    def test_example14(slf):
+        slf.assertEqual(evaluate(parse(example14)), 0)
+
+    def test_example15(slf):
+        slf.assertEqual(evaluate(parse(example15)), 1)
 
 
 if __name__ == "__main__":
--- d16.py
+++ d16.py
@@ -2,17 +2,30 @@
 
 """Advent of Code 2021, Day 16"""
 
+from functools import reduce
+from operator import mul
+
 from aoc import solve
 
-
-def parse(data):
-    return bytes.fromhex(data)
+ops = {
+    0: sum,
+    1: lambda x: reduce(mul, x),
+    2: min,
+    3: max,
+    5: lambda x: int(x[0] > x[1]),
+    6: lambda x: int(x[0] < x[1]),
+    7: lambda x: int(x[0] == x[1])
+}
 
 
 def biterate(bytes):
     for byte in bytes:
         for i in range(7, -1, -1):
             yield (byte & (1 << i)) >> i
+
+
+def parse(data):
+    return biterate(bytes.fromhex(data))
 
 
 def take(bits, n):
@@ -38,26 +51,42 @@ def literal(bits):
     return lit
 
 
-def packet(bits):
+def version_sum(bits):
     ver = number(bits, 3)
     if number(bits, 3) == 4:
         literal(bits)
     elif number(bits, 1):
         for i in range(number(bits, 11)):
-            ver += packet(bits)
+            ver += version_sum(bits)
     else:
         subs = take(bits, number(bits, 15))
         while True:
             try:
-                ver += packet(subs)
+                ver += version_sum(subs)
             except RuntimeError:
                 break
     return ver
 
 
-def version_sum(transmission):
-    return packet(biterate(transmission))
+def evaluate(bits):
+    number(bits, 3)
+    type_id = number(bits, 3)
+    if type_id == 4:
+        return literal(bits)
+    else:
+        operands = []
+        if number(bits, 1):
+            for i in range(number(bits, 11)):
+                operands.append(evaluate(bits))
+        else:
+            subs = take(bits, number(bits, 15))
+            while True:
+                try:
+                    operands.append(evaluate(subs))
+                except RuntimeError:
+                    break
+        return ops[type_id](operands)
 
 
 if __name__ == "__main__":
-    solve(16, parse, version_sum)
+    solve(16, parse, version_sum, evaluate)
