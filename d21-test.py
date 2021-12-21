bc94ad9e87790a306bff736db19ca9b133377fd6
Alex Gulliver <randdalf@gmail.com>
2021-12-21 13:59:01 +0000

    Day 21, Puzzle 2

--- d21-test.py
+++ d21-test.py
@@ -4,15 +4,20 @@
 
 import unittest
 
-from d21 import parse, dirac_dice
+from d21 import parse, practice_dirac_dice, real_dirac_dice
 
 example1 = """Player 1 starting position: 4
 Player 2 starting position: 8"""
 
 
-class DiraceDiceTests(unittest.TestCase):
+class PracticeDiraceDiceTests(unittest.TestCase):
     def test_example1(slf):
-        slf.assertEqual(dirac_dice(parse(example1)), 739785)
+        slf.assertEqual(practice_dirac_dice(parse(example1)), 739785)
+
+
+class RealDiraceDiceTests(unittest.TestCase):
+    def test_example1(slf):
+        slf.assertEqual(real_dirac_dice(parse(example1)), 444356092776315)
 
 
 if __name__ == "__main__":
--- d21.py
+++ d21.py
@@ -2,6 +2,9 @@
 
 """Advent of Code 2021, Day 21"""
 
+from collections import Counter
+from itertools import product
+
 from aoc import solve
 
 
@@ -9,7 +12,7 @@ def parse(data):
     return [int(line.split(': ')[1]) - 1 for line in data.split('\n')]
 
 
-def dirac_dice(state, spaces=10, rolls=3, sides=100, win_score=1000):
+def practice_dirac_dice(state, spaces=10, rolls=3, sides=100, win_score=1000):
     turn = 0
     turns = 0
     scores = [0, 0]
@@ -19,13 +22,43 @@ def dirac_dice(state, spaces=10, rolls=3, sides=100, win_score=1000):
         for i in range(rolls):
             moves += 1 + die
             die = (die + 1) % sides
-        state[turn] = (state[turn] + moves) % spaces
-        scores[turn] += 1 + state[turn]
+        state[turn] += moves
+        scores[turn] += 1 + state[turn] % spaces
         turns += 1
         if scores[turn] >= win_score:
             return turns * min(scores) * rolls
         turn = 1 - turn
 
 
+quantum_die = Counter(
+    3 + i + j + k for i, j, k in product(range(3), range(3), range(3))
+)
+
+
+def diracursive(key, cache):
+    if key in cache:
+        return cache[key]
+    wins = [0, 0]
+    for moves, n in quantum_die.items():
+        turn = key[0]
+        state = list(key[1:3])
+        scores = list(key[3:5])
+        state[turn] += moves
+        scores[turn] += 1 + state[turn] % 10
+        if scores[turn] >= 21:
+            wins[turn] += n
+        else:
+            result = diracursive((1 - turn, *state, *scores), cache)
+            wins[0] += n * result[0]
+            wins[1] += n * result[1]
+    wins = tuple(wins)
+    cache[key] = wins
+    return wins
+
+
+def real_dirac_dice(state):
+    return max(diracursive((0, state[0], state[1], 0, 0), {}))
+
+
 if __name__ == "__main__":
-    solve(21, parse, dirac_dice)
+    solve(21, parse, practice_dirac_dice, real_dirac_dice)
